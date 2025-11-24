5a703b6c76eee048446ad31a5f4aff0ec46a9cc5
Sagittarius-B-Astro <84148121+Sagittarius-B-Astro@users.noreply.github.com>
2025-11-23 21:12:49 -0600

    Added Nelder-Mead method over 2d

--- PlaneChange.py
+++ PlaneChange.py
@@ -5,7 +5,7 @@ def PlaneChange():
     orbit2params = (r2a, r2p, i2, RAAN2, w2)
     grid = loopOverOrbits(orbit1params, orbit2params)
     coarse1, coarse2 = minCoords(grid)
-    point1, point2, deltaV = corasetoFine(coarse1, coarse2)
+    point1, point2, deltaV = NelderMead2d(coarse1, coarse2)
     plotTrajectory(point1, point2)
 
     def loopOverOrbits(init, final):
@@ -73,10 +73,10 @@ def PlaneChange():
     
     def lambertIzzoMethod()
     
-    def brent1d(a, b, function):
+    def Brent1d(a, b, func):
         tolerance = np.power(10, -5)
         max_steps = 1000
-        fa, fb = function(a), function(b)
+        fa, fb = func(a), func(b)
 
         if fa * fb > 0: return "Interval not bracketed properly"
 
@@ -87,17 +87,19 @@ def PlaneChange():
         steps = 0
 
         while steps < max_steps and np.abs(a-b) > tolerance:
-            fa, fb, fc = function(a), function(b), function(c)
+            fa, fb, fc = func(a), func(b), func(c)
 
             if (fa != fc) and (fb != fc):
                 s = (a * fb * fc) / ((fa - fb) * (fa - fc)) + (b * fa * fc) / ((fb - fa) * (fb - fc)) + (c * fa * fb) / ((fc - fa) * (fc - fb))
             else: s = b - fb * (b - a) / (fb - fa)
 
             if (((s - (3 * a + b) / 4) * (s - b)) > 0) or (mflag and np.abs(s - b) >= np.abs(b - c) / 2) or (not mflag and np.abs(s - b) >= np.abs(c - d) / 2) or 
-                (mflag and np.abs(b - c) < np.abs(tolerance)) or (not mflag and np.abs(c - d) < np.abs(tolerance)): mflag = True
+                (mflag and np.abs(b - c) < np.abs(tolerance)) or (not mflag and np.abs(c - d) < np.abs(tolerance)): 
+                s = (a + b) / 2
+                mflag = True
             else: mflag = False
 
-            fs = function(s)
+            fs = func(s)
             d, c = c, b
 
             if fa * fs < 0: b = s
@@ -109,6 +111,69 @@ def PlaneChange():
 
         return b, steps  
  
-    def coarsetoFine()
+    def NelderMead2d(simplex, func):
+        # The function should expect a numpy array representing the point?  
+        tolerance = np.power(10, -3)
+        max_steps = 1000
 
+        alpha = 1 # alpha > 1
+        gamma = 2 # gamma > 1
+        rho = 0.5 # rho in [0, 0.5]
+        sigma = 0.5 # sigma
+        
+        x1, x2, x3 = simplex #WLOG
+        func_array = [(x1, func(x1)), (x2, func(x2)), (x3, func(x3))] # an element is this array is formatted as (nparray representing point, scalar representing minDeltaV)
+        steps = 0
+        
+        while steps < max_steps and np.std([func_array[i][1] for i in range(3)]) > tolerance:
+            # Order
+            sorted(func_array, key=lambda item: item[1])
+            x1, x2, x3 = [func_array[i][0] for i in range(3)]
+            x0 = np.mean(np.array([x1, x2]), axis = 0)
+
+            # Reflect
+            xr = x0 + alpha * (x0 - x3)
+            fxr = func(xr)
+            if fxr >= func_array[0][1] and fxr < func_array[1][1]: 
+                func_array[2] = (xr, fxr)
+                continue
+
+            # Expansion
+            if fxr < func_array[0][1]:
+                xe = x0 + gamma * (xr - x0)
+                fxe = func(xe)
+                if fxe < fxr: 
+                    func_array[2] = (xe, fxe))
+                    continue
+                else: 
+                    func_array[2] = (xr, fxr)
+                    continue
+
+            # Contraction
+            if fxr < func_array[2][1]:
+                xc = x0 + rho * (xr - x0)
+                fxc = func(xc)
+                if fxc < fxr:
+                    func_array[2] = (xc, fxc)
+                    continue
+                else: 
+                    #Shrink
+                    for i, (xi, fxi) in ennumerate(func_array[1:2], start = 1):
+                        xi = x1 + sigma * (xi - x1)
+                        fxi = func(xi)
+                        func_array[i] = (xi, fxi)
+            else:
+                xc = x0 + rho * (x3 - x0)
+                fxc = func(xc)
+                if fxc < fx3:
+                    func_array[2] = (xc, fxc)
+                    continue
+                else: 
+                    #Shrink
+                    for i, (xi, fxi) in ennumerate(func_array[1:2], start = 1):
+                        xi = x1 + sigma * (xi - x1)
+                        fxi = func(xi)
+                        func_array[i] = (xi, fxi)
+        return func_array[0] # the minimum point and the corresponding value of the function
+    
     return deltaV
